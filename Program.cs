6166e3762689eb1e27e7f911d3d0986d9eeff738
Alexander Carabet <alexandercara84@gmail.com>
2023-10-30 17:14:06 +0100

    main

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+    private const string url = "http://söderfix.com";
+    private static readonly string folderPath = Path.Combine("WebSitesData");
+   
+
+    private static readonly string filePath = Path.Combine(folderPath, "söderfix.html");
+
+    static async Task Main(string[] args)
+    {
+        Directory.CreateDirectory(folderPath);  // Skapar mappen
+        await DownloadAndSaveHtmlAsync(url, filePath);
+    }
+
+    static async Task DownloadAndSaveHtmlAsync(string url, string filePath)
+    {
+        using HttpClient client = new();
+        try
+        {
+            string html = await client.GetStringAsync(url);
+            await File.WriteAllTextAsync(filePath, html);
+            Console.WriteLine($"HTML-koden har sparats till {filePath}");
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Ett fel uppstod: {e.Message}");
+        }
+    }
+}
